429ec25719cb3ae2fc0bb73ecb360371a1f62cc4
Soham <1260soham@gmail.com>
2024-07-10 21:19:03 +0530

    Changed to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import Leaderboard from './components/Leaderboard';
 
